12873c1132c331019fff5be1b467092044c6e454
agrgal <aurelio@seritium.es>
2020-01-02 16:24:20 +0000

    Corrigiendo el nombre de la carpeta

--- Cap4_dataframes/df_pearson_1.R
+++
@@ -1,5 +0,0 @@
-l1 = lm(Hijos~Padres,data=df_pearson)
-plot(df_pearson)
-abline(l1)
-
-summary(l1) 
---
+++ Cap5_dataframes/df_pearson_1.R
@@ -0,0 +1,11 @@
+l1 = lm(Hijos~Padres,data=df_pearson)
+plot(df_pearson)
+abline(l1)
+
+summary(l1) 
+
+str(df_pearson)
+names(df_pearson)
+rownames(df_pearson)
+dimnames(df_pearson)
+dim(df_pearson)
